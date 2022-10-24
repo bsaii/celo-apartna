bb7078bcac9145df412de73eb11427d08722cc9e
saii <bernardsaii20@gmail.com>
2022-10-24 23:31:17 +0000

    deploy!: deployed contract with code refactor...

--- contracts/Apartna.sol
+++ contracts/Apartna.sol
@@ -25,7 +25,7 @@ contract Apartna {
     struct ListApartment {
         address payable owner;
         string name;
-        string[] images;
+        string image;
         string description;
         string[] details;
         string[] facilities;
@@ -34,7 +34,7 @@ contract Apartna {
     }
 
     struct Availability {
-        uint256 _id;
+        uint256 _id; // the apartment
         bool allowRent;
         bool allowPurchase;
         bool rented;
@@ -56,13 +56,14 @@ contract Apartna {
         address payable bidder;
         uint256 amount;
         string message;
+        bool approved;
         bool declined;
     }
 
     event ListedAnApartment();
     event updatedAllowPurchase();
     event updatedPrice();
-    event updatedImages();
+    event updatedImage();
     event deletedListedApartment();
     event madeAnOffer();
     event approvedOffer();
@@ -70,6 +71,7 @@ contract Apartna {
     event discountedPrice();
     event updatedDiscountedPrice();
     event endedDiscount();
+    event NightsLeft();
     event rentedAnApartment();
     event terminatedRent();
 
@@ -83,8 +85,7 @@ contract Apartna {
 
     // Modifier to check that the caller is the owner of apartment
     modifier onlyOwner(uint256 _index) {
-        ListApartment storage apartment = listApartment[_index];
-        require(msg.sender == apartment.owner, "Not owner");
+        require(msg.sender == listApartment[_index].owner, "Not owner");
         _;
     }
 
@@ -94,9 +95,18 @@ contract Apartna {
         _;
     }
 
+    // Modifier to check that the apartment has been rented or sold
+    modifier _availability(uint256 _index) {
+        Availability storage available = availability[_index];
+        require(available._id == _index, "Availability does not exist");
+        require(!available.rented, "Apartment already rented");
+        require(!available.sold, "Apartment already sold");
+        _;
+    }
+
     // Modifier to check that an offer was made
-    modifier madeOffer(uint256 _index) {
-        require(_madeOffer[_index], "An offer was not made");
+    modifier madeOffer(uint256 _offerId) {
+        require(_madeOffer[_offerId], "An offer was not made");
         _;
     }
 
@@ -108,13 +118,18 @@ contract Apartna {
 
     /**
      * @dev List an apartment
-     * @param _name: apartment name, _description: description of the apartment
-     * _details: array of details, _facilities: array of facilities, _location: location of the apartment
-     * _price: price of rent per night
+     * @param _name: apartment name
+     * @param _image: image of the apartment
+     * @param _description: description of the apartment
+     * @param _details: array of details
+     * @param _facilities: array of facilities
+     * @param _location: location of the apartment
+     * @param _price: price of rent per night
+     * @param _allowPurchase: bool to allow purchase
      */
     function listAnApartment(
         string memory _name,
-        string[] memory _images,
+        string memory _image,
         string memory _description,
         string[] memory _details,
         string[] memory _facilities,
@@ -125,7 +140,7 @@ contract Apartna {
         listApartment[listedApartments] = ListApartment(
             payable(msg.sender),
             _name,
-            _images,
+            _image,
             _description,
             _details,
             _facilities,
@@ -152,6 +167,14 @@ contract Apartna {
     /**
      * @dev Return listed apartment details
      * @param _index: the index of the listed apartment
+     * @return description
+     * @return details
+     * @return facilities
+     * @return image
+     * @return location
+     * @return name
+     * @return payable(address)
+     * @return price
      */
     function getListedApartment(uint _index)
         public
@@ -161,7 +184,7 @@ contract Apartna {
             string memory description,
             string[] memory details,
             string[] memory facilities,
-            string[] memory images,
+            string memory image,
             string memory location,
             string memory name,
             address payable,
@@ -173,7 +196,7 @@ contract Apartna {
             apartment.description,
             apartment.details,
             apartment.facilities,
-            apartment.images,
+            apartment.image,
             apartment.location,
             apartment.name,
             apartment.owner,
@@ -183,19 +206,30 @@ contract Apartna {
 
     /**
      * @dev Returns the number of listed apartments
+     * @return _listedApartments
      */
-    function NumberOfListedApartments() public view returns (uint256) {
+    function NumberOfListedApartments()
+        public
+        view
+        returns (uint256 _listedApartments)
+    {
         return listedApartments;
     }
 
     /**
      * @dev Returns the availability of an apartment
      * @param _index: the desired apartment
+     * @return id
+     * @return allowPurchase
+     * @return allowRent
+     * @return rented
+     * @return sold
      */
     function getAvailablity(uint256 _index)
         public
         view
         returns (
+            uint256 id,
             bool allowPurchase,
             bool allowRent,
             bool rented,
@@ -205,6 +239,7 @@ contract Apartna {
         Availability storage available = availability[_index];
         require(available._id == _index, "Availability does not exist");
         return (
+            available._id,
             available.allowPurchase,
             available.allowRent,
             available.rented,
@@ -216,7 +251,11 @@ contract Apartna {
      * @dev Allow users to purchase the apartment or not
      * @param _index: the index of the apartment to update, _available: the bool
      */
-    function updateAllowPurchase(uint256 _index) public onlyOwner(_index) {
+    function updateAllowPurchase(uint256 _index)
+        public
+        _availability(_index)
+        onlyOwner(_index)
+    {
         Availability storage available = availability[_index];
         require(available._id == _index, "Availability does not exist");
         available.allowPurchase = !available.allowPurchase;
@@ -230,16 +269,14 @@ contract Apartna {
      */
     function purchaseOnly(uint256 _index)
         public
+        _availability(_index)
         onlyOwner(_index)
-        returns (string memory _msg)
     {
         Availability storage available = availability[_index];
         require(available._id == _index, "Availability does not exist");
         if (available.allowRent) {
             available.allowRent = false;
             available.allowPurchase = true;
-        } else {
-            return "Not available for purchase only";
         }
     }
 
@@ -249,33 +286,33 @@ contract Apartna {
      */
     function updatePrice(uint256 _index, uint256 _price)
         public
-        onlyOwner(_index)
         exists(_index)
+        _availability(_index)
+        onlyOwner(_index)
     {
         // Can't update discounted price, end discount first
         require(
             !_discounted[_index],
             "Can't update disocunted price, end discount first"
         );
-        ListApartment storage apartment = listApartment[_index];
-        apartment.price = _price;
+        listApartment[_index].price = _price;
 
         emit updatedPrice();
     }
 
     /**
-     * @dev Update the images of an apartment
-     * @param _index: the apartment to update, _images: the new images
+     * @dev Update the image of an apartment
+     * @param _index: the apartment to update, _image: the new image
      */
-    function updateImages(uint256 _index, string[] memory _images)
+    function updateImage(uint256 _index, string memory _image)
         public
-        onlyOwner(_index)
         exists(_index)
+        _availability(_index)
+        onlyOwner(_index)
     {
-        ListApartment storage apartment = listApartment[_index];
-        apartment.images = _images;
+        listApartment[_index].image = _image;
 
-        emit updatedImages();
+        emit updatedImage();
     }
 
     /**
@@ -284,11 +321,16 @@ contract Apartna {
      */
     function deleteListedApartment(uint256 _index)
         public
-        onlyOwner(_index)
         exists(_index)
+        _availability(_index)
+        onlyOwner(_index)
     {
         _exists[_index] = false;
         delete listApartment[_index];
+        require(
+            availability[_index]._id == _index,
+            "Availability does not exist"
+        );
         delete availability[_index];
 
         emit deletedListedApartment();
@@ -297,20 +339,23 @@ contract Apartna {
     /**
      * @dev Make an offer to purchase the apartment
      * @param _index: the desired apartment
+     * @param _message: message for the owner
      */
     function makeOffer(uint256 _index, string memory _message)
         public
         payable
         exists(_index)
+        _availability(_index)
     {
         ListApartment storage apartment = listApartment[_index];
         Availability storage available = availability[_index];
+        // Check that availability is valid
         require(available._id == _index, "Availability does not exist");
-        // Owner can't make offer
+        // Owner can't make an offer
         require(apartment.owner != msg.sender, "Owner can't make offer");
-        // Check availability
+        // Check availability for purchase
         require(available.allowPurchase, "Not available for purchase");
-        // Check value
+        // Check value of ETH
         require(msg.value > 0, "Can't make an offer for 0 ETH");
 
         _offersMade[offersMade] = OffersMade(
@@ -318,6 +363,7 @@ contract Apartna {
             payable(msg.sender),
             msg.value,
             _message,
+            false,
             false
         );
 
@@ -331,18 +377,23 @@ contract Apartna {
 
     /**
      * @dev Approve an offer made on the apartment
-     * @param _index: the apartment, _offerId: the desired offer
+     * @param _offerId: the desired offer
      */
-    function approveOffer(uint256 _index, uint256 _offerId)
-        public
-        payable
-        onlyOwner(_index)
-        exists(_index)
-        madeOffer(_index)
-    {
+    function approveOffer(uint256 _offerId) public payable madeOffer(_offerId) {
         OffersMade storage offerMade = _offersMade[_offerId];
         ListApartment storage apartment = listApartment[offerMade.apartmentId];
         Availability storage available = availability[offerMade.apartmentId];
+        // Rented or Sold
+        if (available.rented == true) {
+            revert("Apartment already rented");
+        }
+        if (available.sold == true) {
+            revert("Apartment already sold");
+        }
+        // Allow only the owner
+        require(apartment.owner == msg.sender, "Not the Owner");
+        // Offer already approved
+        require(offerMade.approved, "Offer already approved");
         // Check that offer was not declined
         require(!offerMade.declined, "Offer declined, can't approve");
         require(available.allowPurchase, "Not available for purchase");
@@ -351,6 +402,8 @@ contract Apartna {
             offerMade.amount
         );
         require(received, "Offer not received");
+        // approved
+        offerMade.approved = true;
         // transfer ownership
         apartment.owner = offerMade.bidder;
         // apartment purchased
@@ -362,17 +415,28 @@ contract Apartna {
 
     /**
      * @dev Decline an offer made
-     * @param _index: the apartment, _offerId: the offer
+     * @param _offerId: the offer
      */
-    function declineOffer(uint256 _index, uint256 _offerId)
-        public
-        payable
-        onlyOwner(_index)
-        exists(_index)
-        madeOffer(_index)
-    {
-        // Declined the offer
+    function declineOffer(uint256 _offerId) public payable madeOffer(_offerId) {
         OffersMade storage offerMade = _offersMade[_offerId];
+        Availability storage available = availability[offerMade.apartmentId];
+        // Rented or Sold
+        if (available.rented == true) {
+            revert("Apartment already rented");
+        }
+        if (available.sold == true) {
+            revert("Apartment already sold");
+        }
+        // Offer not approved
+        require(!offerMade.approved, "Offer already approved, can't decline");
+        // Offer already declined
+        require(offerMade.declined, "Offer already declined");
+        // Allow only the owner
+        require(
+            listApartment[offerMade.apartmentId].owner == msg.sender,
+            "Not the Owner"
+        );
+        // Declined the offer
         offerMade.declined = true;
         // Transfer back the amount
         bool success = IERC20(cUsdTokenAddress).transfer(
@@ -385,11 +449,18 @@ contract Apartna {
     }
 
     /**
-     * @dev Return the offers made
+     * @dev Return the offer made
+     * @param _offerId: the desired offer
+     * @return amt - the amount of ETH
+     * @return apartmentId = the apartment
+     * @return bidder - the address of the bidder
+     * @return declined - declined the offer(bool)
+     * @return message - message from the bidder
      */
-    function getOffersMade(uint256 _offerId)
+    function getOfferMade(uint256 _offerId)
         public
         view
+        madeOffer(_offerId)
         returns (
             uint256 amt,
             uint256 apartmentId,
@@ -409,13 +480,22 @@ contract Apartna {
     }
 
     /**
+     * @dev Returns the number of offers made
+     * @return _offers - the number of offers
+     */
+    function numberOfOffers() public view returns (uint256 _offers) {
+        return offersMade;
+    }
+
+    /**
      * @dev Discount the price of an apartment
      * @param _index: the apartment to discount price, _discount: the percentage
      */
     function discountPrice(uint256 _index, uint256 _discount)
         public
-        onlyOwner(_index)
         exists(_index)
+        _availability(_index)
+        onlyOwner(_index)
     {
         ListApartment storage apartment = listApartment[_index];
         discountPercentage = _discount;
@@ -430,11 +510,13 @@ contract Apartna {
     /**
      * @dev Update discount percentage
      * @param _index: the apartment to update discount price
+     * @param _newDiscount: the new discount percentage
      */
     function updateDiscountPrice(uint256 _index, uint256 _newDiscount)
         public
-        onlyOwner(_index)
+        _availability(_index)
         discounted(_index)
+        onlyOwner(_index)
     {
         ListApartment storage apartment = listApartment[_index];
         // convert to original price
@@ -454,8 +536,9 @@ contract Apartna {
      */
     function endDisount(uint256 _index)
         public
-        onlyOwner(_index)
+        _availability(_index)
         discounted(_index)
+        onlyOwner(_index)
     {
         _discounted[_index] = false;
         // set the price to the original
@@ -466,8 +549,20 @@ contract Apartna {
     }
 
     /**
+     * @dev Returns the discount percentage
+     * @return _percent - the discount percentage
+     */
+    function getDiscountPercent() public view returns (uint256 _percent) {
+        return discountPercentage;
+    }
+
+    /**
      * @dev Rent an apartment
      * @param _index: the apartment to rent
+     * @param _nights: the apartment to rent
+     * @param _name: the apartment to rent
+     * @param _email: the apartment to rent
+     * @param _number: the apartment to rent
      */
     function rentAnApartment(
         uint256 _index,
@@ -475,12 +570,12 @@ contract Apartna {
         string memory _name,
         string memory _email,
         uint256 _number
-    ) public payable exists(_index) {
+    ) public payable _availability(_index) exists(_index) {
         ListApartment storage apartment = listApartment[_index];
         Availability storage available = availability[_index];
-        // check for availability for rent
+        require(available._id == _index, "Availability does not exist");
+        // check availability for rent
         require(available.allowRent, "This apartment is not available");
-        require(!available.allowPurchase, "This aparment is not available");
         // Check ownership
         require(msg.sender != apartment.owner, "Owner cannot rent");
 
@@ -493,7 +588,6 @@ contract Apartna {
             email: _email,
             number: _number
         });
-        // require(msg.value >= _nights * apartment.price, "Not Enough ETH");
 
         // make payment
         bool paid = IERC20(cUsdTokenAddress).transferFrom(
@@ -521,6 +615,8 @@ contract Apartna {
         Availability storage available = availability[rent.apartmentId];
         require(available.rented, "Not rented");
         nightsSpent = rent.nights - _nightsSpent;
+
+        emit NightsLeft();
     }
 
     /**
@@ -538,7 +634,6 @@ contract Apartna {
         public
         view
         returns (
-            // rented(_index)
             uint256 apartmentId,
             string memory email,
             address landlord,
@@ -560,13 +655,6 @@ contract Apartna {
         );
     }
 
-    // Terminate Rent
-    // 1. The tenant requests a request to terminate the rent agreement
-    // 2. The landlord decides to approved the termination of the rent agreement
-    // NEXT
-    // Add events to the smart contract
-    // Connect the contract to the celo token
-
     /**
      * @dev Terminate rent agreement with charges of 15%
      * @param _index: the rented apartment
@@ -577,8 +665,8 @@ contract Apartna {
         Availability storage available = availability[rent.apartmentId];
         // Aartment is rented
         require(available.rented, "Not rented");
-        // only Owner
-        require(rent.landlord == apartment.owner, "Not Owner");
+        // Only landlord
+        require(rent.landlord == msg.sender, "Not landlord, can't terminate");
         // Set availability
         available.allowRent = true;
         available.rented = false;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,12 +1,14 @@
 import { ethers } from "hardhat";
 
+// Contract deployed to: 0x4E726ba2C7Cad69f54b789c266567af27aD56dCF
+
 async function main() {
   const Apartna = await ethers.getContractFactory("Apartna");
   const apartna = await Apartna.deploy();
 
   await apartna.deployed();
 
-  console.log(`Contract deployed to ${apartna.address}`);
+  console.log(`Contract deployed to: ${apartna.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
